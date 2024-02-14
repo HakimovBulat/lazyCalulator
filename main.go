d47b5155a9d9225430d75a1bb2ca9ff83eae4695
Hakimov Bulat <bulathakimov1704@gmail.com>
2024-02-14 19:28:37 +0300

    Second day

--- main.go
+++ main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/apaxa-go/eval"
-	"go.uber.org/zap"
+	. "github.com/HakimovBulat/lazyCalculator/utils"
+	"github.com/gin-gonic/gin"
 )
 
-func indexHandle(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	if r.Method == "POST" {
-		expression := r.FormValue("name")
-		logger, err := zap.NewDevelopment()
-		if err != nil {
-			fmt.Println("wrong logger")
-		}
-		expressionEval, err := eval.ParseString(expression, "")
-		if err != nil {
-			logger.Error(err.Error())
-		}
-		answer, err := expressionEval.EvalToInterface(nil)
-		logger.Info(fmt.Sprint(answer), zap.String("expression", expression))
-	}
+var db = make(map[string]int)
+
+func setupRouter() *gin.Engine {
+	SetupLogger()
+	router := gin.Default()
+	router.GET("/", inputExpression)
+	router.POST("/", listExpressions)
+	router.LoadHTMLGlob("templates/*")
+	return router
 }
 func main() {
-	http.HandleFunc("/", indexHandle)
-	http.ListenAndServe(":8080", nil)
+	router := setupRouter()
+	if err := router.Run(":8080"); err != nil {
+		Logger.Error(err.Error())
+	}
+}
+
+func inputExpression(c *gin.Context) {
+	c.HTML(200, "index.html", nil)
+
+}
+func listExpressions(c *gin.Context) {
+	math := c.PostForm("math")
+	Logger.Info(math)
+	c.HTML(200, "list.html", nil)
 }
