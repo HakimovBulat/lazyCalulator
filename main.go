cea354886ded3c7db39069cfc08d225969b13331
Hakimov Bulat <bulathakimov1704@gmail.com>
2024-02-17 23:20:02 +0300

    Fourth day

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	. "github.com/HakimovBulat/lazyCalculator/utils"
 	"github.com/apaxa-go/eval"
 	"github.com/gin-gonic/gin"
@@ -9,7 +12,7 @@ import (
 type Expression struct {
 	Id            int
 	StringVersion string
-	Answer        int
+	Answer        string
 	Status        string
 }
 type DataBase struct {
@@ -20,7 +23,10 @@ type DataBase struct {
 var id int = 1
 var db DataBase
 var mapOperatorsTime = map[string]int{
-	"-": 100, "+": 100, "*": 100, "/": 100,
+	"-": 100,
+	"+": 100,
+	"*": 100,
+	"/": 100,
 }
 
 func setupRouter() *gin.Engine {
@@ -28,7 +34,10 @@ func setupRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/", inputExpression)
 	router.POST("/", createExpression)
-	router.GET("/operators", operators)
+	router.GET("/operators", showOperatorsTime)
+	router.PUT("/operators", replaceOperatorsTime)
+	router.GET("/static_operators", operatorsStatic)
+	router.POST("/static_operators", operatorsStatic)
 	router.LoadHTMLGlob("templates/*")
 	return router
 }
@@ -44,21 +53,64 @@ func inputExpression(c *gin.Context) {
 }
 func createExpression(c *gin.Context) {
 	var newExpression Expression
-	math := c.PostForm("math")
-	Logger.Info(math)
-	expr, err := eval.ParseString(math, "")
+	var answer any
+	var err error
+	mathExpression := c.PostForm("math")
+	Logger.Info(mathExpression)
+	expr, err := eval.ParseString(mathExpression, "")
 	if err != nil {
-		newExpression.Status = "cancel"
+		newExpression = Expression{Id: id, StringVersion: mathExpression, Answer: fmt.Sprint(answer), Status: "cancel"}
+		db.Expressions = append(db.Expressions, newExpression)
+		id++
+		c.HTML(200, "index.html", db)
+		return
 	}
-	answer, err := expr.EvalToInterface(nil)
-	if err != nil {
-		newExpression.Status = "cancel"
+	answer, err = expr.EvalToInterface(nil)
+	if err != nil || answer == nil {
+		newExpression = Expression{Id: id, StringVersion: mathExpression, Answer: "not found", Status: "cancel"}
+		db.Expressions = append(db.Expressions, newExpression)
+		id++
+		c.HTML(200, "index.html", db)
+		return
 	}
-	newExpression = Expression{Id: id, StringVersion: math, Answer: answer.(int), Status: "ok"}
+	fmt.Println(answer)
+	newExpression = Expression{Id: id, StringVersion: mathExpression, Answer: fmt.Sprint(answer), Status: "ok"}
 	db.Expressions = append(db.Expressions, newExpression)
 	id++
 	c.HTML(200, "index.html", db)
 }
-func operators(c *gin.Context) {
-	c.HTML(200, "operators.html", nil)
+func showOperatorsTime(c *gin.Context) {
+	c.HTML(200, "operators.html", gin.H{
+		"addition":       mapOperatorsTime["+"],
+		"substraction":   mapOperatorsTime["-"],
+		"multiplication": mapOperatorsTime["*"],
+		"division":       mapOperatorsTime["/"],
+	})
+}
+
+func replaceOperatorsTime(c *gin.Context) {
+	mapOperatorsTime["+"], _ = strconv.Atoi(c.PostForm("addition"))
+	mapOperatorsTime["-"], _ = strconv.Atoi(c.PostForm("substraction"))
+	mapOperatorsTime["*"], _ = strconv.Atoi(c.PostForm("multiplication"))
+	mapOperatorsTime["/"], _ = strconv.Atoi(c.PostForm("division"))
+	c.HTML(200, "operators.html", gin.H{
+		"addition":       mapOperatorsTime["+"],
+		"substraction":   mapOperatorsTime["-"],
+		"multiplication": mapOperatorsTime["*"],
+		"division":       mapOperatorsTime["/"],
+	})
+}
+func operatorsStatic(c *gin.Context) {
+	if c.Request.Method == "POST" {
+		mapOperatorsTime["+"], _ = strconv.Atoi(c.PostForm("addition"))
+		mapOperatorsTime["-"], _ = strconv.Atoi(c.PostForm("substraction"))
+		mapOperatorsTime["*"], _ = strconv.Atoi(c.PostForm("multiplication"))
+		mapOperatorsTime["/"], _ = strconv.Atoi(c.PostForm("division"))
+	}
+	c.HTML(200, "static_operators.html", gin.H{
+		"addition":       mapOperatorsTime["+"],
+		"substraction":   mapOperatorsTime["-"],
+		"multiplication": mapOperatorsTime["*"],
+		"division":       mapOperatorsTime["/"],
+	})
 }
